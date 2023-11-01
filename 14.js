d0ae79eed4297e70d91b8bf71cbf48814644c310
Samuel Sanchez <a348437@uach.mx>
2023-11-01 13:08:26 -0600

    Hacer un programa que imprima el mayo y el menor de una serie de cinco numeros que vamos introduciendo por el teclado

---
+++ 14.js
@@ -0,0 +1,28 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout,
+});
+let max = -Infinity;
+let min = Infinity;
+let c = 1;
+function readCon() {
+    rl.question("Ingrese un número ("+c+"/5): ", (v) => {
+        if(isNaN(+v)) {
+            readCon();
+            return;
+        }
+        if(+v > max) max = +v;
+        if(+v < min) min = +v;
+        c++;
+        if(c > 5) {
+            rl.close();
+            console.log("Máximo: "+max);
+            console.log("Mínimo: "+min);
+            return;
+        }
+        readCon();
+    });
+}
+readCon();
