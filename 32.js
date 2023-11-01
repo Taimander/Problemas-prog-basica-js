fc194a659f082849fb97782a8dc9c14d68108b39
Samuel Sanchez <a348437@uach.mx>
2023-11-01 13:20:47 -0600

    Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de lista debiendo ir cada uno en una fila distinta

---
+++ 32.js
@@ -0,0 +1,13 @@
+let nombres = [
+    "Gerardo Garza Quintana", "Manuel González Quevedo",
+    "Andrea Villegas Villareal", "Joaquín Hernández Miramontes",
+    "Antonio García Chacón", "Sandra Ruíz Lozano",
+    "Jaime Aguilar Soto", "Roberta Mora López",
+    "Alejandra Ramírez Rojas", "Sofía Cabrera Sosa",
+    "Antonio Torres Flores", "Melissa Rodríguez Ramos",
+    "Natalia Díaz Mendoza", "Ernesto Fernández Castillo",
+    "Cristina Castro Vázquez", "Jennifer Delgado Rivera",
+    "Valeria Salcido Olivas", "Elizabeth Cadena Aguirre",
+    "Diana Vélez Granados", "José Ríoz Nuñez"
+];
+nombres.forEach(el => console.log(el));
